4a90ed1d283629a010686128303909ba84ea3700
liviiasolo <lidoviktoria@gmail.com>
2025-02-09 14:30:47 +0100

    change config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: `/weather-every-day/`,
+  publicPath: process.env.NODE_ENV === 'production' ? '/weather-every-day/' : '/',
   transpileDependencies: true
 })
 
